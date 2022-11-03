f10003668f367fbc9c5e00041dc50d1f9fd03308
Raulmoyano2970 <raulmoyano2970@gmail.com>
2022-11-03 00:19:00 -0300

    Tarea MT-F2-02

---
+++ src/Data/hotelsshow.js
@@ -0,0 +1,119 @@
+//Esto lo sube el "F2" EN LA RAMA MT-F2-02  "SHOWS HOTELES/CASINOS 2 SHOWS POR CADA HOTEL/CASINO"
+
+let dataHShow = [
+  {
+    //hotel 1 (hay que seleccionar 2 shows por cada hotel)
+    id: "it1",
+    hotelId: "ho1",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user1",
+  },
+  {
+    id: "it2",
+    hotelId: "ho2",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user2",
+  },
+  {
+    //hotel 2 (hay que seleccionar 2 shows por cada hotel)
+    id: "it3",
+    hotelId: "ho3",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user1",
+  },
+  {
+    id: "it4",
+    hotelId: "ho4",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user4",
+  },
+  {
+    //hotel 3 (hay que seleccionar 2 shows por cada hotel)
+    id: "it5",
+    hotelId: "ho5",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user5",
+  },
+  {
+    id: "it6",
+    hotelId: "ho6",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user7",
+  },
+  {
+    //hotel 4 (hay que seleccionar 2 shows por cada hotel)
+    id: "it7",
+    hotelId: "ho7",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user7",
+  },
+  {
+    id: "it8",
+    hotelId: "ho8",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user8",
+  },
+  {
+    //hotel 5 (hay que seleccionar 2 shows por cada hotel)
+    id: "it9",
+    hotelId: "ho9",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user9",
+  },
+  {
+    id: "it10",
+    hotelId: "ho10",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user10",
+  },
+  {
+    //hotel 6 (hay que seleccionar 2 shows por cada hotel)
+    id: "it11",
+    hotelId: "ho11",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user11",
+  },
+  {
+    id: "it12",
+    hotelId: "ho12",
+    name: "Miranda",
+    photo: "#",
+    price: 100,
+    date: "2022-12-12", //usar constructor date.
+    userId: "user12",
+  },
+];
+export default dataHShow;
